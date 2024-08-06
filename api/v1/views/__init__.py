eabfb22b965d050528f30d4bd268e547e8fbbe61
Yermehyaw <yermehyaw@gmail.com>
2024-08-06 12:12:41 -0700

    Fix imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,14 +3,15 @@
 Imports: Blueprint, index, states
 """
 from flask import Blueprint
-from index import *
-from api.v1.views import states
 
 
 # Create flask Blueprint to be used for all class objects
 app_views = Blueprint('app_views',
-                    __name__, 
+                    __name__,
                     template_folder='templates',
                     static_folder='static'
             )
 # Default url prefix is '/api/v1'
+
+from .index import *
+from .states import *
