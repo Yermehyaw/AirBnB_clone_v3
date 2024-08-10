4c22da057345566c8b19a020ce0b35d138bfeb66
Yermehyaw <yermehyaw@gmail.com>
2024-08-10 16:05:15 +0100

    Add delete and post view methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -26,6 +26,32 @@ def spec_state(state_id):
     state_obj = storage.get(State, state_id)
     if state_obj is None:
         abort(404)
-    state_dict = state_obj.to_dict()
+    state_obj_dict = state_obj.to_dict()
 
-    return jsonify(state_dict), 200
+    return jsonify(state_obj_dict), 200
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """Delete a specified state object in storage"""
+    state_obj = storage.get(State, state_id)
+    if state_obj is None:
+        abort(404)
+    else:
+        storage.delete(state_obj)
+        return jsonify({}), 200
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Create a new state object in storage"""
+    if not request.is_json:
+        abort(400, 'Not a JSON')
+    if 'name' not in request.json:
+        abort(400, 'Missing Name')
+    req_state = request.get_json()
+    new_state = State()
+    new-state.name = req_state['name']
+
+    storage.new(new_state)
+    storage.save()
+
+    return jsonify(new_state.to_dict()), 201
